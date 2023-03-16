5ba2cc8cde28f41e8d59d0cac0e5efebc41d2891
kabba <danielmuthama23@gmail.com>
2023-03-16 21:53:56 +0300

    update files

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'jhu37jcdsg'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-cr9isr4f.eu.auth0.com',           // Auth0 domain
-  clientId: 'so2GI2QmQaak1RzPnksGTsbsX1Nx9Oxv',  // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: 'dev-90kgrs2z.us.auth0.com',           // Auth0 domain
+  clientId: 'EjisxckJQ1N9YCIWn5aEjjiRod5U09a8',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback' // Auth0 callback url
 }
